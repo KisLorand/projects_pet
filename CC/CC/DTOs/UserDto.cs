25b24e67fc6708114531fe686c202b56e2a455a9
KisLorand <kislorand270@gmail.com>
2022-12-13 13:23:09 +0100

    add Password field

--- CC/CC/DTOs/UserDto.cs
+++ CC/CC/DTOs/UserDto.cs
@@ -2,6 +2,8 @@
 {
 	public class UserDto
 	{
-		public string Username { get; set; }
+		public string Username { get; set; } = string.Empty;
+		public string Password { get; set; } = string.Empty;
+
 	}
 }
