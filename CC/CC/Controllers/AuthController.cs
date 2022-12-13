9e6b5ddc6c87e3b74d9c9bfd95b9bcfc5ebc3b78
KisLorand <kislorand270@gmail.com>
2022-12-13 16:23:31 +0100

    add Register and CreatePasswordHash methods

--- CC/CC/Controllers/AuthController.cs
+++ CC/CC/Controllers/AuthController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CC.DTOs;
+using CC.Properties;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
 
 namespace CC.Controllers
 {
@@ -7,5 +10,31 @@ namespace CC.Controllers
 	[ApiController]
 	public class AuthController : ControllerBase
 	{
+		public static User user = new User();
+
+		[HttpPost("register")]
+		public async Task<ActionResult<User>> Register(UserDto userData)
+		{
+			CreatePasswordHash(userData.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+			user.Username = userData.Username;
+			user.PasswordHash = passwordHash;
+			user.PasswordSalt = passwordSalt;
+
+			return Ok(user);
+		}
+
+		//change the hashing in prod
+		private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+		{
+			using (var hmc = new HMACSHA512())
+			{
+				passwordSalt = hmc.Key;
+				passwordHash = hmc.ComputeHash(
+					System.Text.Encoding.UTF8.GetBytes(password)
+					);
+
+			}
+		}
 	}
 }
