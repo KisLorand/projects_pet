56a3628a7b5cc0cfd3b0bd1c33343d1158966671
KisLorand <kislorand270@gmail.com>
2022-12-14 11:07:20 +0100

    fix condition

--- CC/CC/Controllers/AuthController.cs
+++ CC/CC/Controllers/AuthController.cs
@@ -2,6 +2,8 @@
 using CC.Properties;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
 using System.Security.Cryptography;
 
 namespace CC.Controllers
@@ -28,15 +30,17 @@ namespace CC.Controllers
 		[HttpPost("login")]
 		public async Task<ActionResult<string>> Login(UserDto userData)
 		{
-			if (user.Username == userData.Username)
+			if (user.Username != userData.Username)
 			{
 				return BadRequest("User not found");
 			}
 
-			if (VerifyPasswordHash(userData.Password, user.PasswordHash, user.PasswordSalt))
+			if (!VerifyPasswordHash(userData.Password, user.PasswordHash, user.PasswordSalt))
 			{
-				return BadRequest("Wrong Password");
+				return BadRequest(userData.Password);
 			}
+
+			string token = CreateToken(user);
 
 			return Ok("Token");
 		}
@@ -44,26 +48,35 @@ namespace CC.Controllers
 		//change the hashing in prod
 		private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
 		{
-			using (var hmc = new HMACSHA512())
+			using (var hmac = new HMACSHA512())
 			{
-				passwordSalt = hmc.Key;
-				passwordHash = hmc.ComputeHash(
-					System.Text.Encoding.UTF8.GetBytes(password)
-					);
-
+				passwordSalt = hmac.Key;
+				passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
 			}
 		}
 		//
 
 		private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
 		{
-			using (var hmc = new HMACSHA512(passwordSalt))
+			using (var hmac = new HMACSHA512(passwordSalt))
 			{
-				var computedHash = hmc.ComputeHash(
-					System.Text.Encoding.UTF8.GetBytes(password)
-					);
+				var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
 				return computedHash.SequenceEqual(passwordHash);
 			}
+		}
+
+		private string CreateToken(User user)
+		{
+			List<Claim> claims = new List<Claim>
+			{
+				new Claim(ClaimTypes.Name, user.Username)
+				// Id, email should be added
+			};
+
+			var key = new SymmetricSecurityKey();
+
+
+			return "s";
 		}
 
 	}
