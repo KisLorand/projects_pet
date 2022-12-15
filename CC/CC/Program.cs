a4fbb67407f4d224f2ea514e7829eed7c863a9ba
KisLorand <kislorand270@gmail.com>
2022-12-15 13:37:56 +0100

    add authentication to program

--- CC/CC/Program.cs
+++ CC/CC/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+	.AddJwtBearer(options => {
+		options.TokenValidationParameters = new TokenValidationParameters
+		{
+			ValidateIssuerSigningKey = true,
+			IssuerSigningKey = new SymmetricSecurityKey(
+				System.Text.Encoding.UTF8.GetBytes(
+					builder.Configuration.GetSection("AppSettings : Token").Value))
+		};
+	});
 
 var app = builder.Build();
 
