49c1d5514d8254913291661f61f01f2c9edaa40d
KisLorand <kislorand270@gmail.com>
2022-12-15 16:17:41 +0100

    add Service registration

--- CC/CC/Program.cs
+++ CC/CC/Program.cs
@@ -1,3 +1,4 @@
+using CC.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
@@ -10,6 +11,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+// services
+builder.Services.AddScoped<IUserService, UserService>();
+//
+
 builder.Services.AddSwaggerGen(options => {
 	options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
 	{
