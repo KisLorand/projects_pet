e21860e5375c922687eae48f243af74f7a8cf8ce
KisLorand <kislorand270@gmail.com>
2023-01-02 13:26:54 +0100

    add httpContextAccessor property

--- CC/CC/Services/UserService.cs
+++ CC/CC/Services/UserService.cs
@@ -2,6 +2,12 @@
 {
 	public class UserService : IUserService
 	{
+		private readonly IHttpContextAccessor _httpContextAccessor;
+		public UserService(IHttpContextAccessor httpContextAccessor)
+		{
+			_httpContextAccessor = httpContextAccessor;
+		}
+
 		public string GetUserName()
 		{
 			throw new NotImplementedException();
