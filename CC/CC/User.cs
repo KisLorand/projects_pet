f59ff9989ec3325d6019ad19920b51376cea5e68
KisLorand <kislorand270@gmail.com>
2023-01-03 12:09:26 +0100

    add DateTime properties for creation and expiration

--- CC/CC/User.cs
+++ CC/CC/User.cs
@@ -6,5 +6,7 @@
 		public byte[] PasswordHash { get; set; }
 		public byte[] PasswordSalt { get; set; }
 		public string RefreshToken { get; set; } = string.Empty;
+		public DateTime TokenCreated { get; set; }
+		public DateTime TokenExpires { get; set; }
 	}
 }
