0b165292a26b99f0836c068024c54f43fe4494fc
KisLorand <kislorand270@gmail.com>
2023-01-03 12:06:37 +0100

    add RefreshToken property to User class

--- CC/CC/User.cs
+++ CC/CC/User.cs
@@ -5,5 +5,6 @@
 		public string Username { get; set; } = string.Empty;
 		public byte[] PasswordHash { get; set; }
 		public byte[] PasswordSalt { get; set; }
+		public string RefreshToken { get; set; } = string.Empty;
 	}
 }
