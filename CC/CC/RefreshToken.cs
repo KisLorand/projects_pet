c71649f2548087f46ed78fc6f74adb9588e75aed
KisLorand <kislorand270@gmail.com>
2023-01-03 12:08:53 +0100

    add comment for future implementation

--- CC/CC/RefreshToken.cs
+++ CC/CC/RefreshToken.cs
@@ -3,6 +3,7 @@ namespace CC
 {
 	public class RefreshToken
 	{
+		//public int Id { get; set; } for db storage, connected to a user
 		public string Token { get; set; } = string.Empty;
 		public DateTime Created { get; set; } = DateTime.Now;
 		public DateTime Expires { get; set; } 
