c2acd9871bb1c445edcc411064926ed5ee656b5c
KisLorand <kislorand270@gmail.com>
2023-01-05 16:28:20 +0100

    add RefreshTokenDTO

---
+++ CC/CC/DTOs/RefreshTokenDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace CC.DTOs
+{
+	public class RefreshTokenDTO
+	{
+		public int Id { get; set; }
+		public int UserId { get; set; }
+		public string Token { get; set; } = string.Empty;
+		public DateTime Created { get; set; } = DateTime.Now;
+		public DateTime Expires { get; set; }
+	}
+}
