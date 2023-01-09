b1a8c60cfd8484e129c673f1e00b1b9d9759861b
KisLorand <kislorand270@gmail.com>
2023-01-09 11:07:47 +0100

    add Seed data

--- CC/CC.Infrastructure/CCSeed.cs
+++ CC/CC.Infrastructure/CCSeed.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using CC.DTOs;
 using Microsoft.EntityFrameworkCore;
 namespace CC.Infrastructure
 {
@@ -19,13 +21,31 @@ namespace CC.Infrastructure
             _context.Database.Migrate();
             if (!_context.Users.Any())
             {
-                _context.AddRange(
-                    new User
-                    {
-                        Username = "John",
-                        //PasswordHash = "s"
-
-                    });
+                using (var hmac = new HMACSHA512())
+                {
+                    _context.AddRange(
+                        new User
+                        {
+                            Username = "John",
+                            PasswordHash = hmac.Key,
+                            PasswordSalt = hmac.ComputeHash(Encoding.UTF8.GetBytes("wasd"))
+                        });
+                }
+                _context.SaveChanges();
+            }
+            if (!_context.UserTokens.Any())
+            {
+                using (var hmac = new HMACSHA512())
+                {
+                    _context.AddRange(
+                        new RefreshTokenDTO
+                        {
+                            UserId = (_context.Users.Where(x => x.Username == "John").FirstOrDefault()).Id,
+                            Token = hmac.ComputeHash(Encoding.UTF8.GetBytes("s")).ToString(),
+                            Created = DateTime.Now,
+                            Expires = DateTime.Now
+                        });
+                }
                 _context.SaveChanges();
             }
         }
