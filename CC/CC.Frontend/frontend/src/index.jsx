92ae504ebb984849d7cd93f44780f4c8fba8ed58
KisLorand <kislorand270@gmail.com>
2023-01-11 14:35:57 +0100

    add index

--- CC/CC.Frontend/frontend/src/index.jsx
+++ CC/CC.Frontend/frontend/src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import { AuthProvider } from './contexts/AuthProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
