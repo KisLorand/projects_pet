f6253db8e3ffc11ec8a791d4b9845469e0af4328
KisLorand <kislorand270@gmail.com>
2023-01-16 14:14:43 +0100

    add InputContainer test use

--- CC/CC.Frontend/frontend/src/components/Registration.jsx
+++ CC/CC.Frontend/frontend/src/components/Registration.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useRef, useEffect } from 'react';
+import InputContainer from './InputContainer';
 
 const Registration = () => {
     const url = "https://localhost:44309/api/Auth/register";
@@ -42,16 +43,25 @@ const Registration = () => {
   return (
     <div className="form">
      <form onSubmit={handleSubmit}>
+
        <div className="input-container">
          <label>Username </label>
          <input type="text" name="uname" required />
          {/* {renderErrorMessage("uname")} */}
        </div>
+
        <div className="input-container">
          <label>Password </label>
          <input type="password" name="pass" required />
          {/* {renderErrorMessage("pass")} */}
        </div>
+
+        <InputContainer
+            labelText="Pwd"
+            inputName="pwd"
+            inputVariable={newUser.password}
+        />
+
        <div className="button-container">
          <input type="submit" value="Submit"/>
        </div>
