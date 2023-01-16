3ce8bb99a25b7aaceb23514f38839555610a3257
KisLorand <kislorand270@gmail.com>
2023-01-16 12:42:48 +0100

    add static registration data and fetch

--- CC/CC.Frontend/frontend/src/components/Registration.jsx
+++ CC/CC.Frontend/frontend/src/components/Registration.jsx
@@ -2,15 +2,33 @@ import React from 'react'
 import { useRef, useEffect } from 'react';
 
 const Registration = () => {
-    const url = "";
+    const url = "https://localhost:44309/api/Auth/register";
     const input = useRef(false);
     const errMsg = "error";
+    const newUser = {
+        "username": "newUser",
+        "password": "secretpassword"
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        try {
+            const response = await fetch(url,{
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(newUser),
+              });
+            if (!response.ok) throw Error('Error occoured. Reload the app');
+        } catch (error) {
+            errMsg = error.message;
+        } finally {
+            return  errMsg;
+        }
     }
 
-    useEffect( async () => {
+    /* useEffect( async () => {
         try {
             const response = await fetch(url);
             if (!response.ok) throw Error('Error occoured. Reload the app');
@@ -19,7 +37,7 @@ const Registration = () => {
         } finally {
             return  errMsg;
         }
-    }, [input])
+    }, [input]) */
 
   return (
     <div className="form">
