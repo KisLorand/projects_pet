a2fa5f82b496b08b2604b8fab80baf4cd70f4dec
KisLorand <kislorand270@gmail.com>
2023-01-17 16:08:47 +0100

    add icons for input field

--- CC/CC.Frontend/frontend/src/components/Register.jsx
+++ CC/CC.Frontend/frontend/src/components/Register.jsx
@@ -6,17 +6,16 @@ const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 const REGISTER_URL = '/register';
 
-import React from 'react'
 
 const Register = () => {
     const userRef = useRef();
     const errRef = useRef();
 
     const [user, setUser] = useState('');
-    const [validNam, setValidName] = useState(false);
+    const [validName, setValidName] = useState(false);
     const [userFocus, setUserFocus] = useState(false);
 
-    const [pwd, setPwd] = useSate('');
+    const [pwd, setPwd] = useState('');
     const [validPwd, setValidPwd] = useState(false);
     const [pwdFocus, setPwdFocus] = useState(false);
 
@@ -57,9 +56,39 @@ const Register = () => {
     }, [user, pwd, matchPwd])
 
   return (
-    <div>
-
-    </div>
+    <section>
+        <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+        <h1>Register</h1>
+        <form>
+            <label htmlFor="username">
+                Username:
+                <span className={validName ? "valid" : "offscreen"}>
+                    <FontAwesomeIcon icon={faCheck} />
+                </span>
+                <span className={validName || !user ? "hide" : "invalid"}>
+                    <FontAwesomeIcon icon={faTimes} />
+                </span>
+            </label>
+            <input
+                type="text"
+                id="username"
+                ref={userRef}           //set focus on input
+                autoComplete="off"      // do not suggest previous usernames -> unhelpful
+                onChange={(e) => setUser(e.target.value)}
+                required
+                aria-invalid={validName ? "false" : "true"}
+                aria-describedby="uidnote" // a <p>, that describes the field
+                onFocus={()=> setUserFocus(true)}
+                onBlur={()=> setUserFocus(false)} // when leaving the input field
+            />
+            <p id="uidnote" className={userFocus && user && !validName ? "instructions" : "offscreen"}>
+                <FontAwesomeIcon icon={faInfoCircle}/>
+                4 to 24 characters. <br />
+                Must begin with a letter. <br/>
+                Letters, numbers, underscores, hyphens allowed.
+            </p>
+        </form>
+    </section>
   )
 }
 
