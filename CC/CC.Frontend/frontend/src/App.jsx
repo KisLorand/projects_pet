6a6229eb264807732e077bf3d14b5c93d8f7ab72
KisLorand <kislorand270@gmail.com>
2023-01-18 11:17:59 +0100

    change Registration component to Register

--- CC/CC.Frontend/frontend/src/App.jsx
+++ CC/CC.Frontend/frontend/src/App.jsx
@@ -3,13 +3,14 @@ import Layout from './components/Layout';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Registration from './components/Registration';
+import Register from './components/Register';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="home" element={<Home/>}></Route>
-        <Route path="registration" element={<Registration/>}></Route>
+        <Route path="registration" element={<Register/>}></Route>
       </Route> 
     </Routes>
   );
