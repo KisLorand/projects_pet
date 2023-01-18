1f32c0e2e61095c060054a797ab69ea23c291fbe
KisLorand <kislorand270@gmail.com>
2023-01-18 11:46:03 +0100

    add login route

--- CC/CC.Frontend/frontend/src/App.jsx
+++ CC/CC.Frontend/frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Registration from './components/Registration';
 import Register from './components/Register';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route path="home" element={<Home/>}></Route>
         <Route path="registration" element={<Register/>}></Route>
+        <Route path="login" element={<Login/>}></Route>
       </Route> 
     </Routes>
   );
