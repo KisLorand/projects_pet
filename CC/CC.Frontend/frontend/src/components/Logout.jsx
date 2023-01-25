589abdefb7c9ce5a9b8188ff4cd7ba0007fe4ae9
KisLorand <kislorand270@gmail.com>
2023-01-25 14:50:02 +0100

    add aria-label to logout component

--- CC/CC.Frontend/frontend/src/components/Logout.jsx
+++ CC/CC.Frontend/frontend/src/components/Logout.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useRef, useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
 
 const Logout = () => {
+    const userRef = useRef();
+    const errRef = useRef();
+
+    useEffect(() => {
+        //userRef.current.focus();
+        localStorage.removeItem("userToken");
+    }, [])
+
   return (
-    <div>Logged out</div>
+    <section>
+        <h1 aria-describedby="logoutnote">Logged out</h1>
+        <p id="logoutnote" className="offscreen">
+            You have logged out from the site. <br/>
+            You can go back to the home page.
+        </p>
+        <p>
+            <span className="line">
+                <Link to='/home'>Back to Home</Link>
+            </span>
+        </p>
+    </section>
   )
 }
 
