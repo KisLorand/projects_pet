eebf3113fc01088535a37013b57c3f071625c565
KisLorand <kislorand270@gmail.com>
2023-01-25 15:06:59 +0100

    add fetch base to Logout component

--- CC/CC.Frontend/frontend/src/components/Logout.jsx
+++ CC/CC.Frontend/frontend/src/components/Logout.jsx
@@ -1,12 +1,31 @@
 import { useRef, useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 
+const LOGOUT_URL = 'https://localhost:44309/api/Auth/logout';
+
 const Logout = () => {
     const userRef = useRef();
     const errRef = useRef();
 
-    useEffect(() => {
+    useEffect( async () => {
         //userRef.current.focus();
+        try {
+            if (!localStorage.getItem('userToken')) throw Error("Not Logged in.");
+            const response = await fetch(LOGOUT_URL,{
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(
+                    {
+                        "userToken" : localStorage.getItem('userToken'),
+                        "logoutTime" : Date.now()
+                    }
+                ),
+              });
+        } catch (error) {
+            
+        }
         localStorage.removeItem("userToken");
     }, [])
 
