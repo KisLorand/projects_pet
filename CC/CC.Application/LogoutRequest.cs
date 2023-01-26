bc7361e5a300e8ee4e6b37bf98e08be1da797176
KisLorand <kislorand270@gmail.com>
2023-01-26 10:26:12 +0100

    add LogoutRequest class

---
+++ CC/CC.Application/LogoutRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC.Application
+{
+	public class LogoutRequest
+	{
+		public RefreshToken userToken {get;set;}
+		public DateTime logoutTime {get;set;}
+	}
+}
