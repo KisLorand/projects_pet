fec73f36cfccf1ee6f39ea66b857096e4b6b6b8f
KisLorand <kislorand270@gmail.com>
2023-01-26 11:37:26 +0100

    add SetTokenExpiration

--- CC/CC/Services/ITokenService.cs
+++ CC/CC/Services/ITokenService.cs
@@ -7,5 +7,6 @@ namespace CC.Services
 		List<RefreshTokenDTO> GetTokenByUserId(int userId);
 
 		RefreshTokenDTO GetRefreshToken(int userId, string token);
+		RefreshTokenDTO SetTokenExpiration(string token, DateTime logoutTime);
 	}
 }
