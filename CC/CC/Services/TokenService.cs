45186b03bf7d19dae9e5c914028a66663dd264f4
KisLorand <kislorand270@gmail.com>
2023-01-26 10:50:38 +0100

    add TokenService class, GetTokenByUserId declaration

---
+++ CC/CC/Services/TokenService.cs
@@ -0,0 +1,18 @@
+﻿namespace CC.Services
+{
+	public class TokenService : ITokenService
+	{
+		private readonly IHttpContextAccessor _httpContextAccessor;
+		private readonly ICCContext _dbContext;
+		public TokenService(IHttpContextAccessor httpContextAccessor, ICCContext context)
+		{
+			_httpContextAccessor = httpContextAccessor;
+			_dbContext = context;
+		}
+
+		public RefreshToken GetTokenByUserId(int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
