8c3549e8cfff8c33897186fa8c67888852383f0e
KisLorand <kislorand270@gmail.com>
2023-02-09 10:43:38 +0100

    add protected routes to App

--- CC/CC.Frontend/frontend/src/App.jsx
+++ CC/CC.Frontend/frontend/src/App.jsx
@@ -6,15 +6,25 @@ import Registration from './components/Registration';
 import Register from './components/Register';
 import Login from './components/Login';
 import Logout from './components/Logout';
+import RequireAuth from './components/RequireAuth';
+import Unauthorized from './components/Unautorized';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+
+        {/* public routes */} 
         <Route path="home" element={<Home/>}></Route>
         <Route path="registration" element={<Register/>}></Route>
         <Route path="login" element={<Login/>}></Route>
-        <Route path="logout" element={<Logout/>}></Route>
+        <Route path="unauthorized" element={<Unauthorized/>}></Route>
+
+        {/* protected routes */} 
+        <Route element={<RequireAuth />}> 
+          <Route path="logout" element={<Logout/>}></Route>
+        </Route>
+
       </Route> 
     </Routes>
   );
