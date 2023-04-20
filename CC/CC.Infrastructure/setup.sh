8cc97df9154ec5dccfda4a6cb05240be8f94c3c7
KisLorand <kislorand270@gmail.com>
2023-04-20 23:35:21 +0200

    change setup.sh, currently unused

--- CC/CC.Infrastructure/setup.sh
+++ CC/CC.Infrastructure/setup.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-set -e
-mssql start
-mssql < /src/CCSeed.sql
-service mssql stop
+
+# currently unused
+
+#run the setup script to create the DB and the schema in the DB
+#do this in a loop because the timing for when the SQL instance is ready is indeterminate
+for i in {1..50};
+do
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Yukon900 -d master -i src/CCSeed.sql
+    if [ $? -eq 0 ]
+    then
+        echo "setup.sql completed"
+        break
+    else
+        echo "not ready yet..."
+        sleep 1
+    fi
+done
+
+
+#import the data from the csv file
+# /opt/mssql-tools/bin/bcp DemoData.dbo.Products in "/usr/src/app/Products.csv" -c -t',' -S localhost -U sa -P Yukon900
