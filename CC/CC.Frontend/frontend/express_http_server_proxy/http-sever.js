8d0228a46f8438c062801b0711c344d91458de0a
KisLorand <kislorand270@gmail.com>
2023-05-03 14:11:11 +0200

    add http-server proxy

---
+++ CC/CC.Frontend/frontend/express_http_server_proxy/http-sever.js
@@ -0,0 +1,11 @@
+// javascript
+
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+app.use('/api', createProxyMiddleware({ target: 'http://backend:80', changeOrigin: true }));
+app.listen(3000);
+
+// http://localhost:3000/api/foo/bar -> http://www.example.org/api/foo/bar
