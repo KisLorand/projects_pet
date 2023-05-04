2cd9962c1cdc23c2bfcff320959f9ee303cb77fc
KisLorand <kislorand270@gmail.com>
2023-05-04 16:22:04 +0200

    add test routes to http-server

--- CC/CC.Frontend/frontend/express_http_server_proxy/http-sever.js
+++ CC/CC.Frontend/frontend/express_http_server_proxy/http-sever.js
@@ -7,7 +7,6 @@ const app = express();
 
 app.use('/api', createProxyMiddleware({ target: 'http://backend:80', changeOrigin: true }));
 app.use('/catpng', createProxyMiddleware({ target: 'http://localhost:3000/home', changeOrigin: true }));
-app.use('/api', createProxyMiddleware({ target: 'http://backend:80', changeOrigin: true }));
 app.listen(3000);
 
 // http://localhost:3000/api/foo/bar -> http://www.example.org/api/foo/bar
