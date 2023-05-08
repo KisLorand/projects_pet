333a5bec93916d357ff145f1b46d375b279b74c6
KisLorand <kislorand270@gmail.com>
2023-05-08 15:27:43 +0200

    test router as an option

--- CC/CC.Frontend/frontend/src/express_http_server_proxy/http-sever.js
+++ CC/CC.Frontend/frontend/src/express_http_server_proxy/http-sever.js
@@ -1,23 +1,26 @@
 // javascript
 
 const express = require('express');
+const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 
-app.use(
-  '/api/*', 
-  createProxyMiddleware({ 
-    target: 'http://backend:80',  //target: 'http://backend:5000', 
-    changeOrigin: true, 
-  })
-);
+app.use('/api', require('./router.js'))
+// app.use(
+//   '/api/*', 
+//   createProxyMiddleware({ 
+//     target: 'http://backend:80',  //target: 'http://backend:5000', 
+//     changeOrigin: true, 
+//   })
+// );
 
-//app.use('/catpng', createProxyMiddleware({ target: 'http://localhost:3000/home', changeOrigin: true }));
-app.listen(port, () => {
-    console.log(`http-proxy listening on port ${port}`)
-  })
+// //app.use('/catpng', createProxyMiddleware({ target: 'http://localhost:3000/home', changeOrigin: true }));
+// app.listen(PORT, () => {
+//     console.log(`http-proxy listening on port ${port}`)
+//   })
 
 // http://localhost:3000/api/foo/bar -> http://www.example.org/api/foo/bar
