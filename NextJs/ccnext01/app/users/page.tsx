0160333ebae7d1d6f6a1f115aa1afe7f7bd17846
KisLorand <kislorand270@gmail.com>
2023-05-31 14:01:13 +0200

    add users fetch setup

--- NextJs/ccnext01/app/users/page.tsx
+++ NextJs/ccnext01/app/users/page.tsx
@@ -1,10 +1,33 @@
 import type { Metadata } from 'next'
+import getAllUsers from '@/lib/getAllUsers'
+import Link from 'next/link';
 
 export const metadata: Metadata = {
     title: 'Users'
 }
+//server component; requests during build, before sending to the client
+export default async function UsersPage() {
+    const usersData: Promise<User[]> = getAllUsers();
+    const users = await usersData;
 
-export default function UsersPage() {
+    const content = (
+        <section>
+            <h2>
+                <Link href="/">Back to Home</Link>
+            </h2>
+            <br></br>
+            {users.map(user => {
+                return (
+                    <>
+                        <p key={user.id}>
+                            <Link href={`/users/${user.id}`}></Link>
+                        </p>
+                    </>
+                )
+            })}
+        </section>
+    )
+
   return (
     <div>page</div>
   )
