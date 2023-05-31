cfa90211461d55c9074df0ab8c55bc43e562b09b
KisLorand <kislorand270@gmail.com>
2023-05-31 11:07:15 +0200

    remove css styles temporarily

--- NextJs/ccnext01/app/page.tsx
+++ NextJs/ccnext01/app/page.tsx
@@ -6,8 +6,8 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      {/* <div className={styles.description}>
+    <main>  {/* className={styles.main}>
+     <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
