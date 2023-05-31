0285fe7ae0d61e0949aa13a60f977bb6a3113b97
KisLorand <kislorand270@gmail.com>
2023-05-31 14:36:33 +0200

    add initial User page with fetch

--- NextJs/ccnext01/app/users/[userId]/page.tsx
+++ NextJs/ccnext01/app/users/[userId]/page.tsx
@@ -1,11 +1,27 @@
+import getUser from '@/lib/getUser'
+import getUserPosts from '@/lib/getUserPosts'
+
 type Params = {
     params: {
         userId: string
     }
 }
 
-export default function UserPage({ params: {userId}}: Params) {
+export default async function UserPage({ params: {userId}}: Params) {
+  const userData: Promise<User> = getUser(userId); 
+  const userPostsData: Promise<Post[]> = getUserPosts(userId);
+
+  //the two requests start at the same time; 
+  //awaited together, with Promise.all()
+  const [user, userPosts] = await Promise.all([userData, userPostsData]);
+
   return (
-    <div>page</div>
+    <>
+      <h2>{user.name}</h2>
+      <br/>
+      {/* <UserPosts
+        posts={userPosts}
+      /> */}
+    </>
   )
 }
