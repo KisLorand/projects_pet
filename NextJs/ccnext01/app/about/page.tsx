d5be42e740d9d4033262e11831153c135f1737c5
KisLorand <kislorand270@gmail.com>
2023-05-31 11:49:48 +0200

    add metadata to About page

--- NextJs/ccnext01/app/about/page.tsx
+++ NextJs/ccnext01/app/about/page.tsx
@@ -1,4 +1,10 @@
 import Link from "next/link"
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'About',
+  description: 'Welcome to the About page'
+}
 
 export default function About() {
     return (
