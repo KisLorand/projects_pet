31685288694ee527a877cdae9f6b51b8459c6b3f
KisLorand <kislorand270@gmail.com>
2023-05-31 10:41:26 +0200

    add link to home page

--- NextJs/ccnext01/app/about/page.tsx
+++ NextJs/ccnext01/app/about/page.tsx
@@ -1,5 +1,10 @@
+import Link from "next/link"
+
 export default function About() {
     return (
-        <h1>About</h1>
+        <>
+            <h1>About</h1>
+            <Link href="/">Link to Home Page</Link>
+        </>
     )
 }
