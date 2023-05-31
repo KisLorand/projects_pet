ecf616ea8e78e3378075436e7f3c0ac8ce9ae49f
KisLorand <kislorand270@gmail.com>
2023-05-31 14:19:50 +0200

    add getUser lib function

---
+++ NextJs/ccnext01/lib/getUser.ts
@@ -0,0 +1,6 @@
+
+export default async function getUser(userId: string) {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+    if (!response.ok) throw new Error('Failed to fetch User');
+    return response.json();
+}
