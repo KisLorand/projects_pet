f6161dd26e0197c5a206c52b45c2ff78d5e3ad2c
KisLorand <kislorand270@gmail.com>
2023-05-31 10:58:29 +0200

    add About link to NavBar

--- NextJs/ccnext01/app/navbar/page.tsx
+++ NextJs/ccnext01/app/navbar/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link"
+
 export default function Navbar() {
     return (
         <nav>
-            <h1>Home</h1>
+            <Link href="/"><h1>Home</h1></Link>
+            <Link href="/about"><h1>About</h1></Link>
         </nav>
     )
 }
