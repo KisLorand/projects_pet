ef03c1294044e9fac55785bec9998f187015f94a
KisLorand <kislorand270@gmail.com>
2023-06-01 13:21:49 +0200

    add force-cache to getUserPosts

--- NextJs/ccnext01/lib/getUserPosts.ts
+++ NextJs/ccnext01/lib/getUserPosts.ts
@@ -1,5 +1,5 @@
 export default async function getUserPosts(userId: string) {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`, { cache: 'force-cache'}); // force caching is the default
     if (!response.ok) throw new Error('Failed to fetch data.');
     return response.json();
 }
