3a5ff512763fb70ac1f3b93a2846ac7499619bf1
KisLorand <kislorand270@gmail.com>
2023-06-01 13:34:53 +0200

    add generateStaticParams function to userPage

--- NextJs/ccnext01/app/users/[userId]/page.tsx
+++ NextJs/ccnext01/app/users/[userId]/page.tsx
@@ -3,6 +3,7 @@ import getUserPosts from '@/lib/getUserPosts'
 import { Suspense } from "react"
 import UserPosts from './components/UserPosts'
 import { Metadata } from 'next'
+import getAllUsers from '@/lib/getAllUsers'
 
 type Params = {
     params: {
@@ -39,4 +40,10 @@ export default async function UserPage({ params: {userId}}: Params) {
       </Suspense>
     </>
   )
+}
+
+export async function generateStaticParams() {
+  //next.js deduplicates the data from fetching
+  const usersData: Promise<User[]> = getAllUsers();
+  
 }
