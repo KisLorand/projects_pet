e26b2fdce6cfd182c8c7810a556080190af0647a
KisLorand <kislorand270@gmail.com>
2023-06-01 14:08:42 +0200

    change error throw to return undefined

--- NextJs/ccnext01/lib/getUser.ts
+++ NextJs/ccnext01/lib/getUser.ts
@@ -1,6 +1,6 @@
 
 export default async function getUser(userId: string) {
     const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
-    if (!response.ok) throw undefined;
+    if (!response.ok) return undefined;
     return response.json();
 }
