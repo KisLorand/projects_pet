2dfbb8deccac9279a3b59fc0c0df5636c9fca536
KisLorand <kislorand270@gmail.com>
2023-06-01 13:53:40 +0200

    fix not found pages

--- NextJs/ccnext01/lib/getUser.ts
+++ NextJs/ccnext01/lib/getUser.ts
@@ -1,6 +1,6 @@
 
 export default async function getUser(userId: string) {
     const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
-    if (!response.ok) throw new Error('Failed to fetch User');
+    if (!response.ok) throw undefined;
     return response.json();
 }
