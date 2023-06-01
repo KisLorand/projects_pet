0c346a6faf93f29b9e0f019415f7536cf7348b1a
KisLorand <kislorand270@gmail.com>
2023-06-01 13:26:38 +0200

    add incremental regeneration to getUserPosts

--- NextJs/ccnext01/lib/getUserPosts.ts
+++ NextJs/ccnext01/lib/getUserPosts.ts
@@ -1,5 +1,10 @@
 export default async function getUserPosts(userId: string) {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`, { cache: 'force-cache'}); // force caching is the default
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`, { next: { revalidate: 60 } }); 
+    //cache: 
+    // force-cache is the default
+    // no-store is the opposite, not store the data
+    // incrementaly - ISR - Incramental Static Regeneration
+    // { next: { revalidate: 60 } }
     if (!response.ok) throw new Error('Failed to fetch data.');
     return response.json();
 }
