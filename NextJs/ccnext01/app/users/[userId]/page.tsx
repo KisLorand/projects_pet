73307fcad0dd25473a68fa430f7a6dfcfc0690e5
KisLorand <kislorand270@gmail.com>
2023-06-01 11:34:23 +0200

    add dynamic metadata

--- NextJs/ccnext01/app/users/[userId]/page.tsx
+++ NextJs/ccnext01/app/users/[userId]/page.tsx
@@ -2,11 +2,21 @@ import getUser from '@/lib/getUser'
 import getUserPosts from '@/lib/getUserPosts'
 import { Suspense } from "react"
 import UserPosts from './components/UserPosts'
+import { Metadata } from 'next'
 
 type Params = {
     params: {
         userId: string
     }
+}
+
+export async function generateMetadata({ params: {userId}}: Params): Promise<Metadata> {
+  const userData: Promise<User> = getUser(userId); 
+  const user: User = await userData;
+  return {
+    title: user.name,
+    description: `This is the page of ${user.name}`
+  }
 }
 
 export default async function UserPage({ params: {userId}}: Params) {
